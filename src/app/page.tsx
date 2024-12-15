fdaaf53633f0402eb1890a5a4dc13f920d26fac1
aldjonz <ajon4004@gmail.com>
2024-12-15 22:10:38 +0000

    Add copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,9 @@ export default function Home() {
       {/* <Portfolio /> */}
       <About />
       <Contact />
+      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: 'center', padding: '10px 0', color: 'rgba(255,255,255,0.4)', fontSize: '12px', fontFamily: 'Catamaran, sans-serif'}}>
+        <p>©{new Date().getFullYear()} Aled Jones</p>
+      </div>
     </div>
   );
 }
