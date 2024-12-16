48190ad9ff795ea9de5398bf5c1be8c920b79bd0
aldjonz <ajon4004@gmail.com>
2024-12-16 10:01:44 +0000

    Tweak direction light

--- src/components/NavCanvas/index.tsx
+++ src/components/NavCanvas/index.tsx
@@ -1,8 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
-import Portfolio from '../Portfolio'
-import About from '../About'
-import Expertise from '../Expertise'
+import React from 'react'
 import AnimatedGroups from './AnimatedGroups'
 
 const NavCanvas = () => {
@@ -10,9 +7,8 @@ const NavCanvas = () => {
     <div style={{ position: 'fixed', height: '100vh', top: 0, left: 0, width: '100vw'}}>
         <Canvas gl={{ antialias: true }} dpr={[1, 1.5]} >
             <ambientLight intensity={4} />
-            <directionalLight position={[0, 0, 5]} intensity={4} />
-            <directionalLight position={[-5, 0, 0]} intensity={4} />
-            
+            <directionalLight position={[5, 1, 5]} intensity={2} />
+            <directionalLight position={[-5, -1, 5]} intensity={2} />
             <AnimatedGroups/>
         </Canvas>
     </div>
