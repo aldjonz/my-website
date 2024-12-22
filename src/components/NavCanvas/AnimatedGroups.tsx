42d544fbe4df14fa3c5e8b836dafcc6efd882a62
aldjonz <ajon4004@gmail.com>
2024-12-22 15:37:38 +0000

    Made landing section responsive

--- src/components/NavCanvas/AnimatedGroups.tsx
+++ src/components/NavCanvas/AnimatedGroups.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 import Portfolio from '../Portfolio'
 import About from '../About'
 import Expertise from '../Expertise'
-import { useFrame } from '@react-three/fiber'
+import { useFrame, useThree } from '@react-three/fiber'
 import { Group, Vector3 } from 'three'
 import { OrbitControls } from '@react-three/drei'
 
@@ -17,6 +17,9 @@ const AnimatedGroups = ({ itemActive, setItemActive, textIndex }: { itemActive: 
     const bottomPosition = [-0.2, -2.2, 0]
 
     const startX = 15
+
+    const { viewport } = useThree()
+    const scale = Math.min(1, viewport.width / 16)
 
     const animateActiveItem = (group: Group | null, group1: Group | null, group2: Group | null, group1Y: number, group2Y: number) => {
         if (group && group1 && group2) {
@@ -44,9 +47,9 @@ const AnimatedGroups = ({ itemActive, setItemActive, textIndex }: { itemActive: 
             if (topGroupRef.current && timer > 0.8) {
                 topGroupRef.current.position.x += ((topPosition[0] - topGroupRef.current.position.x) * 0.3)
                 topGroupRef.current.position.y += ((topPosition[1] - topGroupRef.current.position.y) * 0.3)
-                topGroupRef.current.scale.x += (1 - topGroupRef.current.scale.x) * 0.1;
-                topGroupRef.current.scale.y += (1 - topGroupRef.current.scale.y) * 0.1;
-                topGroupRef.current.scale.z += (1 - topGroupRef.current.scale.z) * 0.1;
+                topGroupRef.current.scale.x += (scale - topGroupRef.current.scale.x) * 0.1;
+                topGroupRef.current.scale.y += (scale - topGroupRef.current.scale.y) * 0.1;
+                topGroupRef.current.scale.z += (scale - topGroupRef.current.scale.z) * 0.1;
                 topGroupRef.current.rotation.set(0, 0.15, 0.1)
             }
 
@@ -55,37 +58,50 @@ const AnimatedGroups = ({ itemActive, setItemActive, textIndex }: { itemActive: 
                 middleGroupRef.current.position.y += ((middlePosition[1] - middleGroupRef.current.position.y) * 0.3)
                 middleGroupRef.current.position.z += ((middlePosition[2] - middleGroupRef.current.position.z) * 0.3)
                 middleGroupRef.current.rotation.set(0, -0.15, -0.1)
+                middleGroupRef.current.scale.x += (scale - middleGroupRef.current.scale.x) * 0.1;
+                middleGroupRef.current.scale.y += (scale - middleGroupRef.current.scale.y) * 0.1;
+                middleGroupRef.current.scale.z += (scale - middleGroupRef.current.scale.z) * 0.1;   
             }
 
             if (bottomGroupRef.current && timer > 1.4) {
                 bottomGroupRef.current.position.x += ((bottomPosition[0] - bottomGroupRef.current.position.x) * 0.3)
                 bottomGroupRef.current.position.y += ((bottomPosition[1] - bottomGroupRef.current.position.y) * 0.3)
                 bottomGroupRef.current.position.z += ((bottomPosition[2] - bottomGroupRef.current.position.z) * 0.3)
-                bottomGroupRef.current.scale.x += (1 - bottomGroupRef.current.scale.x) * 0.1;
-                bottomGroupRef.current.scale.y += (1 - bottomGroupRef.current.scale.y) * 0.1;
-                bottomGroupRef.current.scale.z += (1 - bottomGroupRef.current.scale.z) * 0.1;
+                bottomGroupRef.current.scale.x += (scale - bottomGroupRef.current.scale.x) * 0.1;
+                bottomGroupRef.current.scale.y += (scale - bottomGroupRef.current.scale.y) * 0.1;
+                bottomGroupRef.current.scale.z += (scale - bottomGroupRef.current.scale.z) * 0.1;
                 bottomGroupRef.current.rotation.set(0, 0.15, 0.01)
             }
         } else {
             if (itemActive === 'about') {
                 animateActiveItem(topGroupRef.current, middleGroupRef.current, bottomGroupRef.current, -15, -15)
+                if (topGroupRef.current) {
+                    topGroupRef.current.scale.x += (scale - topGroupRef.current.scale.x) * 0.1;
+                    topGroupRef.current.scale.y += (scale - topGroupRef.current.scale.y) * 0.1;
+                    topGroupRef.current.scale.z += (scale - topGroupRef.current.scale.z) * 0.1;
+                }
             } else if (itemActive === 'portfolio') {
                 animateActiveItem(middleGroupRef.current, topGroupRef.current, bottomGroupRef.current, 15, -15)
                 if (middleGroupRef.current && middleGroupRef.current.position.z > -4) {
                     middleGroupRef.current.position.z += (-2 - middleGroupRef.current.position.z) * 0.1;
                     if (topGroupRef.current) {
-                        topGroupRef.current.scale.x += (0 - topGroupRef.current.scale.x) * 0.1;
-                        topGroupRef.current.scale.y += (0 - topGroupRef.current.scale.y) * 0.1;
-                        topGroupRef.current.scale.z += (0 - topGroupRef.current.scale.z) * 0.1;
+                        topGroupRef.current.scale.x += (scale - topGroupRef.current.scale.x) * 0.1;
+                        topGroupRef.current.scale.y += (scale - topGroupRef.current.scale.y) * 0.1;
+                        topGroupRef.current.scale.z += (scale - topGroupRef.current.scale.z) * 0.1;
                     }
                     if (bottomGroupRef.current) {
-                        bottomGroupRef.current.scale.x += (0 - bottomGroupRef.current.scale.x) * 0.1;
-                        bottomGroupRef.current.scale.y += (0 - bottomGroupRef.current.scale.y) * 0.1;
-                        bottomGroupRef.current.scale.z += (0 - bottomGroupRef.current.scale.z) * 0.1;
+                        bottomGroupRef.current.scale.x += (scale - bottomGroupRef.current.scale.x) * 0.1;
+                        bottomGroupRef.current.scale.y += (scale - bottomGroupRef.current.scale.y) * 0.1;
+                        bottomGroupRef.current.scale.z += (scale - bottomGroupRef.current.scale.z) * 0.1;
                     }
                 }
             } else if (itemActive === 'expertise') {
                 animateActiveItem(bottomGroupRef.current, topGroupRef.current, middleGroupRef.current, 15, 15)
+                if (bottomGroupRef.current) {
+                    bottomGroupRef.current.scale.x += (scale - bottomGroupRef.current.scale.x) * 0.1;
+                    bottomGroupRef.current.scale.y += (scale - bottomGroupRef.current.scale.y) * 0.1;
+                    bottomGroupRef.current.scale.z += (scale - bottomGroupRef.current.scale.z) * 0.1;
+                }
             }
         }
     })
