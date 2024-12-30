20c3b36519fd1bb28faa9f5d5de19e17149dde1d
aldjonz <ajon4004@gmail.com>
2024-12-30 17:44:16 +0000

    Removed pointer events from copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <TopographyBg />
       <NavCanvas />
       <Contact />
-      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: 'center', padding: '10px 0', color: 'rgba(255,255,255,0.4)', fontSize: '12px', fontFamily: 'Catamaran, sans-serif'}}>
+      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: 'center', padding: '10px 0', color: 'rgba(255,255,255,0.4)', fontSize: '12px', fontFamily: 'Catamaran, sans-serif', pointerEvents: 'none' }}>
         <p>©{new Date().getFullYear()} Aled Jones</p>
       </div>
     </div>
