d4e0efdc393972905011d619f0bd20e369564f91
aldjonz <ajon4004@gmail.com>
2025-01-09 11:43:07 +0000

    Added swipe on mobile projects

--- src/components/ui/TextScramble.tsx
+++ src/components/ui/TextScramble.tsx
@@ -1,12 +1,14 @@
+import { useLoading } from '@/context/loadingContext';
 import React, { useEffect, useRef, useState } from 'react';
 
 interface TextScrambleProps {
   baseText: string;
-  wrongText: string;
-  correctText: string;
+  wrongText?: string;
+  correctText?: string;
 }
 
 const TextScramble: React.FC<TextScrambleProps> = ({ baseText, wrongText, correctText }) => {
+  const { isLoading } = useLoading();
   const [displayText, setDisplayText] = useState('');
   const [phase, setPhase] = useState<'typing-wrong' | 'deleting' | 'typing-correct' | 'complete'>('typing-wrong');
   const [cursorVisible, setCursorVisible] = useState(true);
@@ -16,9 +18,14 @@ const TextScramble: React.FC<TextScrambleProps> = ({ baseText, wrongText, correc
     
     const animate = () => {
       if (phase === 'complete') return;
-
+      if (!correctText || !wrongText) {
+        setDisplayText(baseText.slice(0, currentIndex.current + 1));
+        currentIndex.current++;
+        setTimeout(animate, 200);
+        return;
+      };
       if (phase === 'typing-wrong') {
-        if (currentIndex.current < (baseText.length + wrongText.length) + 1) {
+        if (currentIndex.current < (baseText?.length + wrongText?.length) + 1) {
           setDisplayText((`${baseText} ${wrongText}`).slice(0, currentIndex.current + 1));
           currentIndex.current++;
           setTimeout(animate, 200);
@@ -39,8 +46,8 @@ const TextScramble: React.FC<TextScrambleProps> = ({ baseText, wrongText, correc
             currentIndex.current = baseText.length;
           }, 600);
         }
-        } else if (phase === 'typing-correct') {
-            if (currentIndex.current < (baseText.length + correctText.length) + 1) {
+      } else if (phase === 'typing-correct') {
+        if (currentIndex.current < (baseText.length + correctText.length) + 1) {
           setDisplayText((`${baseText} ${correctText}`).slice(0, currentIndex.current + 1));
           currentIndex.current++;
           setTimeout(animate, 200);
@@ -49,13 +56,15 @@ const TextScramble: React.FC<TextScrambleProps> = ({ baseText, wrongText, correc
         }
       }
     };
-
-    if (displayText.length === 0) { 
+    if (!correctText || !wrongText) {
+      setTimeout(animate, 6000);
+    } else if (displayText.length === 0) { 
       setTimeout(animate, 3000);
     } else {
       setTimeout(animate, 1000);
     }
-  }, [baseText, wrongText, correctText, phase]);
+    
+  }, [baseText, wrongText, correctText, phase, isLoading]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -65,11 +74,14 @@ const TextScramble: React.FC<TextScrambleProps> = ({ baseText, wrongText, correc
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <p style={{ minWidth: `${Math.max(
+
+  const minWidth = baseText && wrongText && correctText ? `${Math.max(
       baseText.length + wrongText.length,
       baseText.length + correctText.length
-    )}ch` }}>
+    )}ch` : baseText.length;
+
+  return (
+    <p style={{ minWidth }}>
       {displayText}
       <span style={{ color: 'white', opacity: cursorVisible ? 1 : 0 }}>|</span>
     </p>
